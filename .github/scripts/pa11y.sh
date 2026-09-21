f1bc8636d37822156534d75c0da1ffd446d5349e
Sonya Castro <sonya.castro.gomez@gmail.com>
2026-09-20 21:17:08 -0500

    wip: add checks

---
+++ .github/scripts/pa11y.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -u
+
+output=$(npm run pa11y-ci:sitemap 2>&1)
+npm_status=$?
+
+printf '%s\n' "$output"
+
+case "$output" in
+  *"errno 2"*)
+    echo "::error::The site is failing accessibility tests. Please review the comment in the pull request or the pa11y-ci step in the workflow for details."
+    exit 1
+    ;;
+esac
+
+exit "$npm_status"
